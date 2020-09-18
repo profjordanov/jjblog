c430c8ff20c3047fa8541e26e078b4c3708d33c4
Jordan Jordanov <d_yugioh@abv.bg>
2020-09-18 16:44:48 +0300

    blog.api v1.0.1

---
+++ Blog/Blog.Api/Domain/Entities/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Api.Domain.Entities
+{
+    public class Article
+    {
+        public Guid Id { get; set; }
+
+        [MaxLength(1000)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+    }
+}
